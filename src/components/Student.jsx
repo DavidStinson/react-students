62918adb09c82344ed85e62d7b8772ce49337728
David Stinson <david.stinson.5@gmail.com>
2020-03-31 18:48:19 -0500

    complete deliverable

---
+++ src/components/Student.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Score from './Score'
+
+const Student = (props) => (
+  <div>
+    <h2>Name: {props.name}</h2>
+    <p>Bio: {props.bio}</p>
+    <div>
+    { props.scores.map((score, idx) => <
+          Score 
+            key={idx}  
+            date={score.date}
+            score={score.score}
+          />
+    ) }
+    </div>
+  </div>
+);
+ 
+export default Student;
+
+
